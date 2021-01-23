f7bc92094a6734954c2b71195f555e4a67c34422
eren23 <erenak1996@gmail.com>
2021-01-23 17:11:38 +0100

    Player mechanics added

---
+++ src/entities/Player.js
@@ -0,0 +1,86 @@
+import Phaser from "phaser";
+import initAnimations from "../entities/playerAnims";
+import collidable from "../mixins/collidable";
+
+class Player extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y, "player");
+
+    scene.add.existing(this); // our players sprites
+    scene.physics.add.existing(this); // physics
+
+    //Mixins
+    Object.assign(this, collidable); // what we do here is basically  we copy all the collidable properties to this
+
+    this.init();
+    this.initEvents();
+  }
+
+  //Customs
+
+  init() {
+    this.gravity = 500;
+    this.playerSpeed = 200;
+    this.cursors = this.scene.input.keyboard.createCursorKeys();
+    this.jumpCount = 0;
+    this.concecutiveJumps = 1;
+    //if you want to access to scene from a arcade super class you need to define it like that, otherwise "this" will refer to player
+
+    this.body.setGravityY(this.gravity);
+    this.setCollideWorldBounds(true);
+    this.setOrigin(1, 1);
+
+    initAnimations(this.scene.anims);
+  }
+
+  initEvents() {
+    this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
+    // (eventtofireafter, functiontocall, context)
+    //execution above is a trick to get update function from a scene, Arcade Sprite usually don't have such a thing
+  }
+
+  update() {
+    const { left, right, space, up } = this.cursors;
+    const isSpaceJustDown = Phaser.Input.Keyboard.JustDown(space); // only returns true pey key press
+    const isUpJustDown = Phaser.Input.Keyboard.JustDown(up); // only returns true pey key press
+    const onFloor = this.body.onFloor();
+
+    if (left.isDown) {
+      this.setVelocityX(-this.playerSpeed);
+      this.setFlipX(true);
+    } else if (right.isDown) {
+      this.setVelocityX(this.playerSpeed);
+      this.setFlipX(false);
+    } else {
+      this.setVelocityX(0);
+    }
+
+    if ((isSpaceJustDown || isUpJustDown) && (onFloor || this.jumpCount < this.concecutiveJumps)) {
+      this.setVelocityY(-this.playerSpeed * 1.4);
+      this.jumpCount++;
+    }
+    if (onFloor) {
+      this.jumpCount = 0;
+    }
+
+    onFloor ? (this.body.velocity.x !== 0 ? this.play("run", true) : this.play("idle", true)) : this.play("jump", true);
+    //dont play it again if it's playing
+    //second value ignore if playing
+  }
+}
+
+export default Player;
+
+//Life Cycle
+//   preUpdate(time, delta) {
+//     super.preUpdate(time, delta); // it's a good practice to call preupdate on the super class, otherwise it may fail during the animations and so on
+//     const { left, right } = this.cursors;
+
+//     if (left.isDown) {
+//       this.setVelocityX(-this.playerSpeed);
+//     } else if (right.isDown) {
+//       this.setVelocityX(this.playerSpeed);
+//     } else {
+//       this.setVelocityX(0);
+//     }
+//   }
--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -1,38 +1,79 @@
 import Phaser from "phaser";
+import Player from "../entities/Player";
 
 class PlayScene extends Phaser.Scene {
-  constructor() {
+  constructor(config) {
     super("PlayScene");
+    this.config = config;
   }
 
   create() {
-    const map = this.createMap()
-    const layers= this.createLayers(map)
-    const player = this.createPlayer()
-    this.physics.add.collider(player, layers.platforms)
+    const map = this.createMap();
+    const layers = this.createLayers(map);
+    const playerZones = this.getPlayerZones(layers.playerZones);
+    const player = this.createPlayer(playerZones);
+
+    this.createPlayerColliders(player, {
+      colliders: {
+        platformsColliders: layers.platformsColliders,
+      },
+    });
+
+    this.setupFollowUpCamera(player);
+    this.createEndOfLevel(playerZones.end, player);
   }
 
-  createMap(){
+  update() {}
+
+  //Customs
+  createMap() {
     const map = this.make.tilemap({ key: "map" });
     map.addTilesetImage("main_lev_build_1", "tiles-1");
-    // const tileset2 = map.addTilesetImage("main_lev_build_2", "tiles-2");
-
-    return map
+    return map;
   }
 
-  createLayers(map){
-    const tileset = map.getTileset("main_lev_build_1")
-   const environment = map.createStaticLayer("environment", tileset);
-   const platforms = map.createStaticLayer("platforms", tileset);
-   platforms.setCollisionByExclusion(-1, true);
-   return {environment, platforms}
+  createLayers(map) {
+    const tileset = map.getTileset("main_lev_build_1");
+
+    const playerZones = map.getObjectLayer("player_zones");
+    const platformsColliders = map.createStaticLayer("platforms_colliders", tileset);
+    const environment = map.createStaticLayer("environment", tileset);
+    const platforms = map.createStaticLayer("platforms", tileset);
+
+    platformsColliders.setCollisionByProperty({ collides: true });
+    return { environment, platforms, platformsColliders, playerZones };
   }
 
-  createPlayer(){
-    const player = this.physics.add.sprite(100,250, "player")
-    player.body.setGravityY(200)
-    player.setCollideWorldBounds(true)
-    return player
+  createPlayer({ start }) {
+    const player = new Player(this, start.x, start.y); // firsts value should be the scene
+    return player;
+  }
+
+  createPlayerColliders(player, { colliders }) {
+    player.addCollider(colliders.platformsColliders);
+  }
+
+  setupFollowUpCamera(player) {
+    const { height, width, mapOffset, zoomFactor } = this.config;
+    this.physics.world.setBounds(0, 0, width + mapOffset, height + 200); //expanding world bounds
+    this.cameras.main.setBounds(0, 0, width + mapOffset, height).setZoom(zoomFactor); // so camera bouns are set, without the parts here it tries to displays irrevelant parts also
+    this.cameras.main.startFollow(player);
+  }
+
+  getPlayerZones(playerZonesLayer) {
+    const playerZones = playerZonesLayer.objects;
+    return {
+      start: playerZones.find((zone) => zone.name === "startZone"),
+      end: playerZones.find((zone) => zone.name === "endZone"),
+    };
+  }
+
+  createEndOfLevel(end, player) {
+    const endOfLevel = this.physics.add.sprite(end.x, end.y, "end").setAlpha(0).setSize(10, 100).setOrigin(0.5, 1);
+    const endOfOverlay = this.physics.add.overlap(player, endOfLevel, () => {
+      endOfOverlay.active = false;
+      console.log("you won");
+    });
   }
 }
 
